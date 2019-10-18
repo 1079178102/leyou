976068cb3b2be8d600e9e2453e1b99e321333f58
XPH <xph@qq.com>
2019-10-18 15:55:56 +0800

    分类管理

--- ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
+++ ly-common/src/main/java/com/leyou/common/enums/ExceptionEnum.java
@@ -8,7 +8,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public enum ExceptionEnum {
-    PRICE_CANNOT_BR_NULL(400,"价格不能为空");
+    PRICE_CANNOT_BR_NULL(400,"价格不能为空"),
+    CATEGORY_NOT_FIND(404,"商品分类没查到"),
+    ;
     private int code;
     private String msg;
 }
